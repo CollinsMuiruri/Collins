9d94d528d94821983681fa8d1e5801f16878b464
Collins Muiruri <wanyekicollins@gmail.com>
2020-12-09 04:10:38 +0300

    Clean up home page + admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_title = models.CharField(max_length=10)
+    project_description = models.CharField(max_length=30)
+    project_image = models.ImageField(upload_to = 'media/')
+    project_url = models.URLField()
+
+    def __str__(self):
+        return self.project_title
+
+    def save_project(self):
+        return self.save
+
+    def delete_project(self):
+        return self.delete
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('test/', views.index, name = 'test'),
+    path('', views.index, name = 'home'),
 ]
